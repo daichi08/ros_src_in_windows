19d32507e1547e155c880478e5aa31290b58609d
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2019-03-27 16:31:45 +0900

    類似度に変更

--- kalman_test/scripts/kalman_test_node.py
+++ kalman_test/scripts/kalman_test_node.py
@@ -3,27 +3,38 @@
 
 import rospy
 import math
+import numpy as np
 from sensor_msgs.msg import LaserScan
 
-obs = []
+obstacles = []
 
 def callback(msg):
-    global obs
-    obs = []
+    global obstacles
+    obstacles = []
     lines_point = []
-    datasize = len(msg.ranges)
-    offset = -90 * math.pi/180
-    before_point = [0, 0]
+    before_vector = msg.ranges[0]
 
-    for i in range(datasize):
-        pass
-    print(msg)
+    for i in range(len(msg.ranges)):
+        current_vector = msg.ranges[i]
+        norm = abs(current_vector - before_vector)
+        similarity = 1/(1+norm)
+        if similarity > 0.9:
+            current_point = [current_vector * math.cos(i*msg.angle_increment), current_vector * math.sin(i*msg.angle_increment)]
+            lines_point.append(current_point)
+        else:
+            if len(lines_point) > 0:
+                obstacles.append(lines_point)
+                lines_point = []
+        before_vector = current_vector
+
+    for obstacle in obstacles:
+        print(obstacle)
 
 if __name__ == '__main__':
     rospy.init_node("scan_values")
     r = rospy.Rate(20)
     sub = rospy.Subscriber("/scan", LaserScan, callback)
 
-    while not rospy.is_shutdown():
-        print(obs)
+    #while not rospy.is_shutdown():
+    for i in range(4):
         r.sleep()
