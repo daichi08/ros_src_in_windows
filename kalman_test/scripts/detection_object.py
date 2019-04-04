69ac0db90573338402cdd1e525f1ad684f6d9aef
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2019-04-04 11:30:07 +0900

    完成

--- kalman_test/scripts/detection_object.py
+++ kalman_test/scripts/detection_object.py
@@ -20,75 +20,76 @@ offset    = 90 * math.pi/180.0
 
 def callback(msg):
     global obstacles
-    obstacles = []
-    datasize  = len(msg.ranges)
-    print(datasize)
+    obstacles   = []
     lines_point = []
-    before_vector = msg.ranges[0]
+    datasize  = len(msg.ranges)
     angle_inc = msg.angle_increment
+
+    before_point = Point(msg.ranges[0]*math.cos(msg.angle_min),msg.ranges[0]*math.sin(msg.angle_min))
 
     for i in range(datasize):
         angle = msg.angle_min + angle_inc * i
         current_vector = msg.ranges[i]
         if not math.isnan(current_vector):
             current_point = Point(current_vector * math.cos(angle), current_vector * math.sin(angle))
-            norm = abs(current_vector - before_vector)
+            norm = math.sqrt(pow(current_point.x-before_point.x, 2)+pow(current_point.y-before_point.y, 2))
             similarity = 1/(1+norm)
 
-            if similarity > 0.92:
+            if similarity > 0.97:
                 lines_point.append(current_point)
             else:
                 obstacles.append(lines_point)
-                lines_point = [current_point]
-        before_vector = current_vector
+                lines_point = []
+        before_point = current_point
 
 def main():
+    rospy.init_node("scan_values")
+    r = rospy.Rate(20)
+    sub = rospy.Subscriber("/scan", LaserScan, callback, queue_size=1)
+    pub = rospy.Publisher("/markers", MarkerArray, queue_size=2)
     while not rospy.is_shutdown():
-        rospy.init_node("scan_values")
-        r = rospy.Rate(20)
-        sub = rospy.Subscriber("/scan", LaserScan, callback, queue_size=1)
-        pub = rospy.Publisher("/markers", MarkerArray, queue_size=2)
         marker_array = MarkerArray()
         index = 0
 
         if obstacles:
             for obstacle in obstacles:
-                marker = Marker()
+                if obstacle:
+                    marker = Marker()
 
-                marker.header.frame_id = "/laser"
-                marker.id = index
-                marker.ns = "object"
-                marker.type = marker.LINE_LIST
-                marker.action = marker.ADD
+                    marker.header.frame_id = "/laser"
+                    marker.id = index
+                    marker.ns = "object"
+                    marker.type = marker.LINE_LIST
+                    marker.action = marker.ADD
 
-                #marker.pose.position.x = 0
-                #marker.pose.position.y = 0
-                #marker.pose.position.z = 0
+                    #marker.pose.position.x = 0
+                    #marker.pose.position.y = 0
+                    #marker.pose.position.z = 0
 
-                marker.pose.orientation.x = 0
-                marker.pose.orientation.y = 0
-                marker.pose.orientation.z = 0
-                marker.pose.orientation.w = 1
+                    marker.pose.orientation.x = 0
+                    marker.pose.orientation.y = 0
+                    marker.pose.orientation.z = 0
+                    marker.pose.orientation.w = 1
 
-                marker.points.append(obstacle[0])
-                marker.points.append(obstacle[-1])
-                #marker.points.append(Point(0,0))
-                #marker.points.append(Point(random.random(),random.random()))
+                    marker.points.append(obstacle[0])
+                    marker.points.append(obstacle[-1])
+                    #marker.points.append(Point(0,0))
+                    #marker.points.append(Point(random.random(),random.random()))
 
-                marker.color.r = 1
-                marker.color.g = 0.0
-                marker.color.b = 0.0
-                marker.color.a = 0.5
+                    marker.color.r = 1
+                    marker.color.g = 0.0
+                    marker.color.b = 0.0
+                    marker.color.a = 0.5
 
-                marker.scale.x = 0.01
-                marker.scale.y = 0
-                marker.scale.z = 0
+                    marker.scale.x = 0.01
+                    marker.scale.y = 0
+                    marker.scale.z = 0
 
-                marker.lifetime = rospy.Duration(1)
+                    marker.lifetime = rospy.Duration(1)
 
-                marker_array.markers.append(marker)
+                    marker_array.markers.append(marker)
 
-                index += 1
+                    index += 1
             pub.publish(marker_array)
         else:
             print("wait")
--- kalman_test/src/marker.cpp
+++ kalman_test/src/marker.cpp
@@ -40,7 +40,7 @@ void callback(const sensor_msgs::LaserScan::ConstPtr& msg){
                         pow(current_point[1]-before_point[1], 2)
                     );
             similarity = 1/(1+norm);
-            if(norm > 0.98){
+            if(similarity > 0.95){
                 linear_points.push_back(current_point);
             }else{
                 obstacles.push_back(linear_points);
@@ -55,20 +55,58 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "marker_node");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("scan", 1, callback);
+    ros::Publisher m_pub = n.advertise<visualization_msgs::MarkerArray>("markers", 100);
     ros::Rate rate(20);
 
     while(ros::ok()){
-        ros::spinOnce();
-        rate.sleep();
-
+        visualization_msgs::MarkerArray marker_array;
         if(obstacles.empty()){
             ROS_WARN("nothing data");
         }else{
-            for(const auto& line : obstacles){
-                for(const auto& point : line){
-                    ROS_INFO("[%f, %f]", point[0], point[1]);
+            for(int i = 0; i < obstacles.size(); i++){
+                if(!obstacles[i].empty()){
+                    visualization_msgs::Marker marker;
+
+                    geometry_msgs::Point first_point, last_point;
+
+                    auto tmp_first_point = obstacles[i].front();
+                    auto tmp_last_point  = obstacles[i].back();
+
+                    first_point.x = tmp_first_point[0];
+                    first_point.y = tmp_first_point[1];
+                    first_point.z = 0;
+                    last_point.x  = tmp_last_point[0];
+                    last_point.y  = tmp_last_point[1];
+                    last_point.z  = 0;
+
+                    marker.header.frame_id = "laser";
+                    marker.id = i;
+                    marker.ns = "object";
+                    marker.type = marker.LINE_LIST;
+                    marker.action = marker.ADD;
+
+                    marker.points.push_back(first_point);
+                    marker.points.push_back(last_point);
+
+                    marker.color.r = 1;
+                    marker.color.g = 0;
+                    marker.color.b = 0;
+                    marker.color.a = 0.8;
+
+                    marker.scale.x = 0.01;
+                    marker.scale.y = 0;
+                    marker.scale.z = 0;
+
+                    marker.lifetime = ros::Duration();
+
+                    marker_array.markers.push_back(marker);
                 }
             }
         }
+        m_pub.publish(marker_array);
+
+        ros::spinOnce();
+        rate.sleep();
     }
 }
+
