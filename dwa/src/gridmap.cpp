9c0b26478c009265697738312201d665fc27e4d1
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2019-04-19 17:46:07 +0900

    きれいにしてる

--- dwa/src/gridmap.cpp
+++ dwa/src/gridmap.cpp
@@ -14,35 +14,19 @@ using namespace std;
 const float GRID_SIZE = 0.1;
 const float ANGLE_RES = 3 * M_PI / 180.0;
 
-vector< vector<float> > grids;
-vector< vector<int> >   obstacle_ids;
-vector<int>             angle_ids;
+vector< vector<float> > gridmap;
 
-void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
-    int i         = 0;
-    int map_range = 0;
-    float angle   = 0.0;
-    float x_p     = 0.0; // real x position
-    float y_p     = 0.0; // real y position
-    int x_i       = 0.0;
-    int y_i       = 0.0;
-    float angle_inc = msg->angle_increment;
-    float angle_min = msg->angle_min;
-    float range_max = msg->range_max;
-    int zero_i      = int(round(range_max/GRID_SIZE));
-    float obstacle_angle = 0.0;
-    float grid_angle     = 0.0;
-    int angle_id      = 0;
-    int grid_angle_id = 0;
-    vector<int>::iterator it;
-
-    map_range = int(ceil(range_max*2/GRID_SIZE));
-    grids.assign(map_range, vector<float>(map_range, 0.0));
-
-    obstacle_ids.clear();
+vector< vector<int> > calcObsGrid(auto ranges, float range_max, float angle_inc, float angle_min){
+    int i       = 0;
+    int x_i     = 0;
+    int y_i     = 0;
+    float x_p   = 0.0;
+    float y_p   = 0.0;
+    float angle = 0.0;
+    vector< vector<int> > obstacle_ids;
 
     // グリッドマップに対して障害物の生成
-    for(const auto range : msg->ranges){
+    for(const auto range : ranges){
         if(!isnan(range) && range != 0){
             angle = angle_min + angle_inc * i;
             x_p = range * cos(angle);
@@ -52,38 +36,67 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
             y_i = int(round((y_p+range_max)/GRID_SIZE));
 
             obstacle_ids.push_back({x_i, y_i});
-            grids[x_i][y_i] = 1;
+            gridmap[x_i][y_i] = 1;
         }
         i += 1;
     }
+
     // 重複削除
     sort(obstacle_ids.begin(), obstacle_ids.end());
     obstacle_ids.erase(unique(obstacle_ids.begin(), obstacle_ids.end()), obstacle_ids.end());
 
-    // 計測できない範囲に対して値代入
-    grids[zero_i][zero_i] = 9;
-    for(auto obstacle_id : obstacle_ids){
-        obstacle_angle = atan2(obstacle_id[1]-zero_i, obstacle_id[0]-zero_i);
-        if(obstacle_angle < 0){
-            obstacle_angle += 2*M_PI;
-        }
-        angle_id = int(floor(obstacle_angle/ANGLE_RES));
-        angle_ids.push_back(angle_id);
-    }
+    return obstacle_ids;
+}
 
-    for(int i = 0; i < map_range; i++){
-        for(int j = 0; j < map_range; j++){
-            grid_angle = atan2(j-zero_i, i-zero_i);
-            if(grid_angle < 0){
-                grid_angle += 2*M_PI;
-            }
-            grid_angle_id = int(floor(grid_angle/ANGLE_RES));
-            it = find(angle_ids.begin(), angle_ids.end(), grid_angle_id);
-            if(it != angle_ids.end()){
-                grids[i][j] += 5;
-            }
-        }
+inline int calcGrid(int range_max,float position){
+    return int(round((position+range_max)/GRID_SIZE));
+
+}
+
+float correctAtan2(int y, int x){
+    float angle = atan2(y, x);
+    if(angle<0)
+        angle += 2*M_PI;
+    return angle;
+}
+
+inline int calcAngleID(float angle){
+    return int(floor(angle/ANGLE_RES));
+}
+
+void precasting(auto obstacle_ids, int origin_grid){
+    float dist     = 0.0;
+    float angle    = 0.0;
+    int angle_id   = 0;
+    int relative_x = 0;
+    int relative_y = 0;
+
+    vector<int>   angle_ids;
+    vector<float> distances;
+
+    for(auto obstacle_id : obstacle_ids){
+        relative_x = obstacle_id[0]-origin_grid;
+        relative_y = obstacle_id[1]-origin_grid;
+
+        dist     = sqrt(relative_x^2 + relative_y^2);
+        angle    = correctAtan2(relative_y, relative_x);
+        angle_id = calcAngleID(angle);
+        angle_ids.push_back(angle_id);
+        distances.push_back(dist);
     }
+}
+
+void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
+    /* 宣言 */
+    float range_max = msg->range_max;
+    float angle_inc = msg->angle_increment;
+    float angle_min = msg->angle_min;
+    int map_range   = int(ceil(range_max*2/GRID_SIZE));
+    int origin_grid = calcGrid(range_max, 0);
+
+    vector< vector<int> > obstacle_ids = calcObsGrid(msg->ranges, range_max, angle_inc, angle_min);
+    gridmap.assign(map_range, vector<float>(map_range, 0.0));
+    //precasting(obstacle_ids, origin_grid);
 }
 
 int main(int argc, char **argv){
@@ -95,9 +108,9 @@ int main(int argc, char **argv){
     ros::Rate rate(1);
 
     while(ros::ok()){
-        if(!grids.empty()){
-            std_msgs::Float32MultiArray gridmap;
-            for(auto grid_list : grids){
+        if(!gridmap.empty()){
+            std_msgs::Float32MultiArray grids;
+            for(auto grid_list : gridmap){
                 for(auto grid : grid_list){
                     cout << grid;
                 }
