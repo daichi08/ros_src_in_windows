b2bbf4ce020a8448bb75d10852306988557c2ddc
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2019-06-13 10:55:28 +0900

    hoge

--- dwa/src/transform_point.cpp
+++ dwa/src/transform_point.cpp
@@ -1,10 +1,5 @@
 #include "ros/ros.h"
-#include "sensor_msgs/Laser.h"
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "transform_point");
-    ros::NodeHandle n;
-
-
-    return 0;
 }
